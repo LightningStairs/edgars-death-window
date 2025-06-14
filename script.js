0c80bb18fd237bfd565d5c4c10587d591bcf9219
Jenah Blitz <jenah.blitz@gmail.com>
2025-06-14 14:21:29 -0400

    add already dead option

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
         'before_window': document.getElementById('count_before_window'),
         'after_window': document.getElementById('count_after_window'),
         'wont_die_young': document.getElementById('count_wont_die_young')
+        'already_dead': document.getElementById('count_already_dead')
     };
 
     const COOLDOWN_DURATION = 60 * 40000; 
